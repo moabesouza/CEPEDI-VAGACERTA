a3164a766e2f684e6f12376dffd88ef93a61ff88
Moabe de jesus souza <moabe.2009@hotmail.com>
2024-03-07 18:44:54 -0300

    criação do user context

---
+++ src/contexts/userContext.tsx
@@ -0,0 +1,33 @@
+import React, { createContext, useContext, useState } from 'react';
+
+import { User } from "../@types/user";
+
+interface UserContextType {
+  user: User | null;
+  setUser: React.Dispatch<React.SetStateAction<User | null>>;
+}
+
+
+export const UserContext = createContext<UserContextType | undefined>(undefined);
+
+interface UserProviderProps {
+  children: React.ReactNode;
+}
+
+export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = (): UserContextType => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error('useUser must be used within a UserProvider');
+  }
+  return context;
+};
